82eec899181f52f2720cd55f0aa92b0f00986157
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-01-08 14:42:25 -0600

    gitignore

--- update_csv.py
+++ update_csv.py
@@ -2,6 +2,7 @@ from pandas import read_csv, concat, DataFrame
 from get_points import get_points
 import os
 from datetime import datetime
+from create_csv import create_csv
 
 
 def update_csv(filename="data.csv"):
@@ -13,13 +14,11 @@ def update_csv(filename="data.csv"):
     Returns:
         df: updated dataframe
     """
+    if not os.path.exists("data.csv"):
+        create_csv(filename)
     points = get_points()
     df = read_csv(filename)
     df.drop(df.filter(regex="Unname"), axis=1, inplace=True)
-    last_points_entry = -1
-    if len(df) >= 1:
-        last_points_entry = dict(df.iloc[-1])["points"]
-
     new_entry = {
         "entry_no.": len(df) + 1,
         "datetime": datetime.now().date(),
