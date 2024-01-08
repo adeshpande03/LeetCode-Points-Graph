99711705f3d9a66db35376ec6f4c0a6be1190ff4
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-01-08 15:48:54 -0600

    main.py

--- generate_graph.py
+++ generate_graph.py
@@ -2,16 +2,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from scipy.interpolate import interp1d
-import datetime
-import matplotlib.dates as mdates
 
 
 def generate_graph(filename="data.csv"):
     LEETCODE_YELLOW = "#ffa115"
     df = pd.read_csv(filename)
     df["points"] = df["points"].astype(float)
-    dates = df["datetime"].tolist()
-    print(dates)
     x0 = range(len(df))
     y0 = df["points"].tolist()
     plt.figure(facecolor="#282828")
@@ -19,7 +15,8 @@ def generate_graph(filename="data.csv"):
     plt.rcParams["axes.labelcolor"] = LEETCODE_YELLOW
     plt.rcParams["xtick.color"] = LEETCODE_YELLOW
     plt.rcParams["ytick.color"] = LEETCODE_YELLOW
-    plt.plot(x0, y0, "o", color="#ffa115", markersize=2)
+    marker_size = 2 if len(df) < 10 else 1
+    plt.plot(x0, y0, "o", color="#ffa115", markersize=marker_size)
     cubic_interpolation_model = interp1d(x0, y0, kind="cubic")
     x = np.linspace(min(x0), max(x0), 5000)
     y = cubic_interpolation_model(x)
@@ -35,7 +32,6 @@ def generate_graph(filename="data.csv"):
 
     plt.yticks(np.arange(min(y0) // 250 * 250 - 250, max(y0) + 250, 250))
     plt.savefig("points_graph.png", bbox_inches="tight", dpi=300)
-    # plt.show()
     return
 
 
---
+++ main.py
@@ -0,0 +1,10 @@
+from update_csv import update_csv
+from generate_graph import generate_graph
+
+
+def main():
+    update_csv()
+    generate_graph()
+
+
+main()
