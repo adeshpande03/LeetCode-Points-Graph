b6e6656ec56ff0876ef893096f97e4563b5154e7
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-01-08 16:53:14 -0600

    fixing graph

--- generate_graph.py
+++ generate_graph.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from scipy.interpolate import interp1d
+import matplotlib.patches as patches
 
 
 def generate_graph(filename="data.csv"):
@@ -10,17 +11,18 @@ def generate_graph(filename="data.csv"):
     df["points"] = df["points"].astype(float)
     x0 = range(len(df))
     y0 = df["points"].tolist()
+
     plt.figure(facecolor="#282828")
     plt.rcParams["text.color"] = LEETCODE_YELLOW
     plt.rcParams["axes.labelcolor"] = LEETCODE_YELLOW
     plt.rcParams["xtick.color"] = LEETCODE_YELLOW
     plt.rcParams["ytick.color"] = LEETCODE_YELLOW
     marker_size = 2 if len(df) < 10 else 1
-    plt.plot(x0, y0, "o", color="#ffa115", markersize=marker_size)
+    plt.plot(x0, y0, "o", color=LEETCODE_YELLOW, markersize=marker_size)
     cubic_interpolation_model = interp1d(x0, y0, kind="cubic")
     x = np.linspace(min(x0), max(x0), 5000)
     y = cubic_interpolation_model(x)
-    plt.plot(x, y, color="#ffa115")
+    plt.plot(x, y, color=LEETCODE_YELLOW)
     plt.box(False)
     plt.tick_params(
         axis="x",
@@ -29,14 +31,17 @@ def generate_graph(filename="data.csv"):
         top=False,
         labelbottom=False,
     )
+
     fig = plt.gcf()
     fig.set_size_inches(8, 2)
+
     if max(y0) < 3000:
         plt.yticks(np.arange(min(y0) // 250 * 250 - 250, max(y0) + 250, 250))
     else:
         plt.yticks(np.arange(min(y0) // 1000 * 1000 - 1000, max(y0) + 1000, 1000))
+
     plt.savefig("points_graph.png", bbox_inches="tight", dpi=500)
-    return
+    return 
 
 
 if __name__ == "__main__":
