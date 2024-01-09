6d80d0d9e8eb9aea269bf946908802500056f374
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-01-08 19:03:06 -0600

    test

--- round_corners.py
+++ round_corners.py
@@ -1,9 +1,11 @@
 from PIL import Image, ImageDraw
+
+
 def add_corners(im, rad):
-    circle = Image.new('L', (rad * 2, rad * 2), 0)
+    circle = Image.new("L", (rad * 2, rad * 2), 0)
     draw = ImageDraw.Draw(circle)
     draw.ellipse((0, 0, rad * 2 - 1, rad * 2 - 1), fill=255)
-    alpha = Image.new('L', im.size, 255)
+    alpha = Image.new("L", im.size, 255)
     w, h = im.size
     alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
     alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, h - rad))
@@ -11,6 +13,8 @@ def add_corners(im, rad):
     alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
     im.putalpha(alpha)
     return im
-im = Image.open('points_graph.png')
-im = add_corners(im, 1000)
-im.save('tiger.png')
+
+
+im = Image.open("points_graph.png")
+im = add_corners(im, 500)
+im.save("tiger.png")
