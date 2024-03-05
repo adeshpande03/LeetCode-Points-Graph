29017a1a42b740f7637e2ae8c092cc513ce61ae1
Akhil Deshpande <deshpandeakhils@gmail.com>
2024-03-05 13:57:56 -0600

    automated LC_SESSION pulling

--- update_csv.py
+++ update_csv.py
@@ -18,7 +18,7 @@ def update_csv(filename="data.csv"):
     if not os.path.exists("data.csv"):
         create_csv(filename)
     lc_session = get_session()
-    print(lc_session)
+    print("lc_session", lc_session)
     points = get_points(lc_session)
     df = read_csv(filename)
     df.drop(df.filter(regex="Unname"), axis=1, inplace=True)
