c4e82b313be35a2c74dca4095fbb0823b3fad65e
Akhil Deshpande <deshpandeakhils@gmail.com>
2024-03-06 19:27:36 -0600

    automated LC_SESSION pulling

--- get_points.py
+++ get_points.py
@@ -16,28 +16,28 @@ def get_points(LEETCODE_SESSION=os.environ.get("LEETCODE_SESSION")):
     status_code = s.status_code
     status_set = {200}
     if status_code not in status_set:
-        CARRIERS = {
-            "tmobile": "@tmomail.net",
-            "verizon": "@vtext.com",
-        }
-        EMAIL = os.environ.get("EMAIL")
-        APP_PASSWORD = os.environ.get("APP_PASSWORD")
-        PHONE_NUMBER = os.environ.get("PHONE_NUMBER")
+    #     CARRIERS = {
+    #         "tmobile": "@tmomail.net",
+    #         "verizon": "@vtext.com",
+    #     }
+    #     EMAIL = os.environ.get("EMAIL")
+    #     APP_PASSWORD = os.environ.get("APP_PASSWORD")
+    #     PHONE_NUMBER = os.environ.get("PHONE_NUMBER")
 
-        def send_message(
-            carrier,
-            message,
-            PHONE_NUMBER=PHONE_NUMBER,
-        ):
-            recipient = PHONE_NUMBER + CARRIERS[carrier]
-            auth = (EMAIL, APP_PASSWORD)
-            server = smtplib.SMTP("smtp.gmail.com", 587)
-            server.starttls()
-            server.login(auth[0], auth[1])
-            server.sendmail(auth[0], recipient, message)
+    #     def send_message(
+    #         carrier,
+    #         message,
+    #         PHONE_NUMBER=PHONE_NUMBER,
+    #     ):
+    #         recipient = PHONE_NUMBER + CARRIERS[carrier]
+    #         auth = (EMAIL, APP_PASSWORD)
+    #         server = smtplib.SMTP("smtp.gmail.com", 587)
+    #         server.starttls()
+    #         server.login(auth[0], auth[1])
+    #         server.sendmail(auth[0], recipient, message)
 
-        message = "Please fix Leetcode Points graph, your LEETCODE_SESSION is invalid"
-        send_message("verizon", message)
+    #     message = "Please fix Leetcode Points graph, your LEETCODE_SESSION is invalid"
+    #     send_message("verizon", message)
 
         raise RuntimeError(
             f"Status code: {status_code} is invalid. Check LEETCODE_SESSION or manually set csrf to troubleshoot."
